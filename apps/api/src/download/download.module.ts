26f1fc0738555c3a01ef3c3a92c5558d28b7cd11
Poomn Yennattee <devilbastopoftheworld@gmail.com>
2022-03-30 23:01:19 +0700

    delete download

--- apps/api/src/download/download.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DownloadService } from './download.service';
-import { DownloadController } from './download.controller';
-
-@Module({
-  controllers: [DownloadController],
-  providers: [DownloadService]
-})
-export class DownloadModule {}
